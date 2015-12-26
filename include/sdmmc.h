cd9ee980c565677c548f342ae7a55907ca6e0fb9
173210 <root.3.173210@live.com>
2015-12-26 21:28:06 +0900

    sdmmc: sdmmc.h: Use enumerators for register definitions

--- include/sdmmc.h
+++ include/sdmmc.h
@@ -8,37 +8,39 @@
 
 #define SDMMC_BASE				0x10006000
 
-#define REG_SDCMD				0x00
-#define REG_SDPORTSEL			0x02
-#define REG_SDCMDARG			0x04
-#define REG_SDSTOP				0x08
-#define REG_SDBLKCOUNT			0x0a
+enum sdmmc_regs {
+	REG_SDCMD				= 0x00,
+	REG_SDPORTSEL			= 0x02,
+	REG_SDCMDARG			= 0x04,
+	REG_SDSTOP				= 0x08,
+	REG_SDBLKCOUNT			= 0x0a,
 
-#define REG_SDRESP0				0x0c
-#define REG_SDRESP1				0x10
-#define REG_SDRESP2				0x14
-#define REG_SDRESP3				0x18
+	REG_SDRESP0				= 0x0c,
+	REG_SDRESP1				= 0x10,
+	REG_SDRESP2				= 0x14,
+	REG_SDRESP3				= 0x18,
 
-#define REG_SDSTATUS			0x1c
+	REG_SDSTATUS			= 0x1c,
 
-#define REG_SDIRMASK			0x20
-#define REG_SDCLKCTL			0x24 
-		
-#define REG_SDBLKLEN			0x26
-#define REG_SDOPT				0x28
-#define REG_SDFIFO				0x30
+	REG_SDIRMASK			= 0x20,
+	REG_SDCLKCTL			= 0x24 ,
 
-#define REG_DATACTL 			0xd8
-#define REG_SDRESET				0xe0
-#define REG_SDPROTECTED			0xf6 //bit 0 determines if sd is protected or not?
+	REG_SDBLKLEN			= 0x26,
+	REG_SDOPT				= 0x28,
+	REG_SDFIFO				= 0x30,
 
-#define REG_DATACTL32 			0x100
-#define REG_SDBLKLEN32			0x104
-#define REG_SDBLKCOUNT32		0x108
-#define REG_SDFIFO32			0x10C
+	REG_DATACTL 			= 0xd8,
+	REG_SDRESET				= 0xe0,
+	REG_SDPROTECTED			= 0xf6, //bit 0 determines if sd is protected or not?
 
-#define REG_CLK_AND_WAIT_CTL	0x138
-#define REG_RESET_SDIO			0x1e0
+	REG_DATACTL32 			= 0x100,
+	REG_SDBLKLEN32			= 0x104,
+	REG_SDBLKCOUNT32		= 0x108,
+	REG_SDFIFO32			= 0x10C,
+
+	REG_CLK_AND_WAIT_CTL	= 0x138,
+	REG_RESET_SDIO			= 0x1e0
+};
 
 #define TMIO_STAT_CMDRESPEND    0x00000001
 #define TMIO_STAT_DATAEND       0x00000004
@@ -109,31 +111,31 @@ extern "C" {
 #endif
 
 //---------------------------------------------------------------------------------
-static inline uint16_t sdmmc_read16(uint16_t reg) {
+static inline uint16_t sdmmc_read16(enum sdmmc_regs reg) {
 //---------------------------------------------------------------------------------
 	return *(volatile uint16_t*)(SDMMC_BASE + reg);
 }
 
 //---------------------------------------------------------------------------------
-static inline void sdmmc_write16(uint16_t reg, uint16_t val) {
+static inline void sdmmc_write16(enum sdmmc_regs reg, uint16_t val) {
 //---------------------------------------------------------------------------------
 	*(volatile uint16_t*)(SDMMC_BASE + reg) = val;
 }
 
 //---------------------------------------------------------------------------------
-static inline uint32_t sdmmc_read32(uint16_t reg) {
+static inline uint32_t sdmmc_read32(enum sdmmc_regs reg) {
 //---------------------------------------------------------------------------------
 	return *(volatile uint32_t*)(SDMMC_BASE + reg);
 }
 
 //---------------------------------------------------------------------------------
-static inline void sdmmc_write32(uint16_t reg, uint32_t val) {
+static inline void sdmmc_write32(enum sdmmc_regs reg, uint32_t val) {
 //---------------------------------------------------------------------------------
 	*(volatile uint32_t*)(SDMMC_BASE + reg) = val;
 }
 
 //---------------------------------------------------------------------------------
-static inline void sdmmc_mask16(uint16_t reg, const uint16_t clear, const uint16_t set) {
+static inline void sdmmc_mask16(enum sdmmc_regs reg, const uint16_t clear, const uint16_t set) {
 //---------------------------------------------------------------------------------
 	uint16_t val = sdmmc_read16(reg);
 	val &= ~clear;
@@ -142,7 +144,7 @@ static inline void sdmmc_mask16(uint16_t reg, const uint16_t clear, const uint16
 }
 
 //---------------------------------------------------------------------------------
-static inline void sdmmc_mask32(uint16_t reg, const uint32_t clear, const uint32_t set) {
+static inline void sdmmc_mask32(enum sdmmc_regs reg, const uint32_t clear, const uint32_t set) {
 //---------------------------------------------------------------------------------
 	uint32_t val = sdmmc_read32(reg);
 	val &= ~clear;
