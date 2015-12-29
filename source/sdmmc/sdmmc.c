53bf889ae868100e1d9c2603171f05665e836e1f
173210 <root.3.173210@live.com>
2015-12-29 19:05:45 +0900

    sdmmc: Update the copyright notice

--- source/sdmmc/sdmmc.c
+++ source/sdmmc/sdmmc.c
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright (c) 2014-2015, Normmatt
+ * Copyright (c) 2014-2015, Normmatt, 173210
  *
  * Alternatively, the contents of this file may be used under the terms
  * of the GNU General Public License Version 2, as described below:
